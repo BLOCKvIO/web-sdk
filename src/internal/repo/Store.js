30a1310df2f97d1dab7be4ac4c786894e36e31ec
Andrew Rothman <andrew@andrewrothman.com>
2018-03-28 11:32:32 -0700

    save asset provider in localstorage

--- src/internal/repo/Store.js
+++ src/internal/repo/Store.js
@@ -1,3 +1,6 @@
+const REFRESH_TOKEN_STORAGE_KEY = "blockv.web_sdk.refresh_token";
+const ASSET_PROVIDER_STORAGE_KEY = "blockv.web_sdk.asset_provider";
+
 export default class Store {
 
     static get server(){
@@ -31,20 +34,28 @@ export default class Store {
     }
 
     static set refreshToken(refresh){
-      window.localStorage.setItem('refresh', refresh);
+      window.localStorage.setItem(REFRESH_TOKEN_STORAGE_KEY, refresh);
     }
 
     static get refreshToken(){
-      let rT = window.localStorage.getItem('refresh');
+      let rT = window.localStorage.getItem(REFRESH_TOKEN_STORAGE_KEY);
       return rT;
     }
 
-    static set assetProvider(provider){
-      this.assetProviderObj = provider;
+    static set assetProvider(provider) {
+      window.localStorage.setItem(ASSET_PROVIDER_STORAGE_KEY, JSON.stringify(provider));
     }
 
-    static get assetProvider(){
-      return this.assetProviderObj;
+    static get assetProvider() {
+      let assetProviderObjStr = window.localStorage.getItem(ASSET_PROVIDER_STORAGE_KEY);
+      
+      // return `undefined` as the asset provider object if it has not
+      // previously been saved in localStorage
+      if (assetProviderObjStr === null) {
+        return undefined;
+      }
+      
+      return JSON.parse(assetProviderObjStr);
     }
 
 
