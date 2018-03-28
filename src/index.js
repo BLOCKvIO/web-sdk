caa575f2b476baf01cd8b01f914ea388733aab03
Andrew Rothman <andrew@andrewrothman.com>
2018-03-28 11:12:47 -0700

    fix Vatoms import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Blockv from "./client/Blockv"
 import UserManager from "./client/manager/UserManager"
-import Vatoms from "./Client/manager/Vatoms"
+import Vatoms from "./client/manager/Vatoms"
 
 module.exports = Blockv;
 module.exports.UserManager = UserManager;
