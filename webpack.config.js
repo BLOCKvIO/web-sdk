633b4e5a8cae4245ea66a7d01ddfee344fb77db3
kyle-mcintosh <kyle@ydangleapps.com>
2018-07-30 13:34:23 +0200

    changed babel to use env instead of presets 2015 and 2017

--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,57 @@
 //
 // WebPack config file
 
-var webpack = require('webpack');
+import webpack from 'webpack';
+import path from 'path';
 
 module.exports = {
-    plugins: [],
-    module: {
-        loaders: []
-    }
+  plugins: [],
+  module: {
+    loaders: [],
+  },
 };
 // The app's starting file
-module.exports.entry = "./src/index.js";
+module.exports.entry = './src/index.js';
 
 // The final app's JS output file
 module.exports.output = {
-    path: __dirname + "/dist/",
-    filename: "blockv-sdk.min.js",
-    libraryTarget:"var",
-    library:"Blockv"
+  path: path.join(__dirname, '/dist/'),
+  filename: 'blockv-sdk.min.js',
+  libraryTarget: 'var',
+  library: 'Blockv',
 };
 // Output a sourcemap
-module.exports.devtool = "source-map";
+module.exports.devtool = 'source-map';
 
 // Compile support for ES6 classes and React etc
 module.exports.module.loaders.push({
-    test: /\.js$/,
-    exclude: /node_modules/,
-    loader: 'babel-loader',
-    query: {
-        presets: ["es2017","es2015"]
-    }
+  test: /\.js$/,
+  exclude: /node_modules/,
+  loader: 'babel-loader',
+  query: {
+    presets: ['env'],
+  },
 });
 
 // Compile support for CSS
 module.exports.module.loaders.push({
-    test: /\.css$/,
-    loaders: ["style", "css"]
+  test: /\.css$/,
+  loaders: ['style', 'css'],
 });
-module.exports.module.loaders.push(
-{
-    test: /(\.scss|\.css)$/,
-    loader: 'style!css!postcss!sass'
+module.exports.module.loaders.push({
+  test: /(\.scss|\.css)$/,
+  loader: 'style!css!postcss!sass',
 });
 
 module.exports.module.loaders.push({
-    test: /(\.png|\.svg|\.jpg)$/,
-    loader: "url-loader"
+  test: /(\.png|\.svg|\.jpg)$/,
+  loader: 'url-loader',
 });
 module.exports.node = {
-        console: true,
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
-    };
+  console: true,
+  fs: 'empty',
+  net: 'empty',
+  tls: 'empty',
+};
 
-module.exports.plugins.push(new webpack.optimize.LimitChunkCountPlugin({maxChunks: 1}));
+module.exports.plugins.push(new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }));
