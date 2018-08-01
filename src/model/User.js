595314dfae20c0cbe0cbea9a108cdf68073483f4
kyle-mcintosh <kyle@ydangleapps.com>
2018-08-01 15:30:03 +0200

    added system properties and meta data to response

--- src/model/User.js
+++ src/model/User.js
@@ -19,5 +19,19 @@ export default class User {
     this.birthday = user.properties.birthday;
     this.language = user.properties.language;
     this.nonPushNotification = user.properties.nonpush_notification;
+    this.guestId = user.properties.guest_id;
+    this.isPasswordSet = user.properties.is_password_set;
+    this.activated = user.system_properties.activated;
+    this.isAdmin = user.system_properties.is_admin;
+    this.isMerchant = user.system_properties.is_merchant;
+    this.lastLogin = user.system_properties.last_login;
+    this.pubFqdn = user.system_properties.pub_fqdn;
+    this.meta = {
+      createdBy: user.meta.created_by,
+      dataType: user.meta.data_type,
+      modifiedBy: user.meta.modified_by,
+      whenCreated: user.meta.when_created,
+      whenModified: user.meta.when_modified,
+    };
   }
 }
