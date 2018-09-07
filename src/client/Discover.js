ed7eb4cb5e979798b5631f07248c5a2d0825182c
kyle-mcintosh <kyle@ydangleapps.com>
2018-09-07 15:04:57 +0200

    added vatom package to Discover query

--- src/client/Discover.js
+++ src/client/Discover.js
@@ -103,7 +103,50 @@ export default class Discover {
   /** Execute the discover query, returning an array of Vatoms. @returns Promise<[Vatom]> */
   execute() {
     // Send request
-    return this.bv.client.request('POST', '/v1/vatom/discover', this.getPayload(), true).then(data => data);
+    return this.bv.client.request('POST', '/v1/vatom/discover', this.getPayload(), true).then((data) => {
+      const { actions, faces, results } = data;
+
+      const actionsArray = [];
+      const facesArray = [];
+      const vatomsArray = [];
+      // eslint-disable-next-line
+      for (let a of actions) {
+        const aName = a.name.split('::Action::');
+        const aKey = aName[0];
+        actionsArray.push({
+          template: aKey,
+          action: aName[1],
+          meta: a.meta,
+          properties: a.properties,
+        });
+      }
+      // eslint-disable-next-line
+      for (let f of faces) {
+        facesArray.push({
+          template: f.template,
+          id: f.id,
+          meta: f.meta,
+          properties: f.properties,
+        });
+      }
+      // eslint-disable-next-line
+      for (let v of results) {
+        const { template } = v['vAtom::vAtomType'];
+        const obj = {
+          id: v.id,
+          private: v.private,
+          unpublished: v.unpublished,
+          version: v.version,
+          when_created: v.when_created,
+          when_modified: v.when_modified,
+          properties: v['vAtom::vAtomType'],
+          faces: facesArray.filter(f => f.template === template),
+          actions: actionsArray.filter(a => a.template === template),
+        };
+        vatomsArray.push(obj);
+      }
+      return vatomsArray;
+    });
   }
 
   /** Execute the discover query, returning the count of vatoms. @returns Promise<Integer> */
