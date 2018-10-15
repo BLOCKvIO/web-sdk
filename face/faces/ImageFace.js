32240c4d99ed14dbe69920b73ec93929387a9cca
Jack McIntosh <kyle@ydangleapps.com>
2018-10-15 14:24:40 +0200

    replaced import with require

--- face/faces/ImageFace.js
+++ face/faces/ImageFace.js
@@ -8,7 +8,7 @@
 //  ANY KIND, either express or implied. See the License for the specific language
 //  governing permissions and limitations under the License.
 //
-import BaseFace from './BaseFace';
+const BaseFace = require('./BaseFace');
 
 export default class ImageFace extends BaseFace {
   onLoad() {
