897a6aa0ddfe93d0622fbce96f39be32ec752fa8
Jack McIntosh <kyle@ydangleapps.com>
2018-10-15 14:28:58 +0200

    replaced import with require

--- src/client/manager/UserManager.js
+++ src/client/manager/UserManager.js
@@ -8,7 +8,7 @@
 //  ANY KIND, either express or implied. See the License for the specific language
 //  governing permissions and limitations under the License.
 //
-import jwtDecode from 'jwt-decode';
+const jwtDecode = require('jwt-decode');
 
 class UserManager {
   constructor(UserApi, store) {
