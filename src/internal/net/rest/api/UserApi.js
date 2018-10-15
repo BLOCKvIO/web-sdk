10690b54612218da041c5e26b062d8c111e285b1
Jack McIntosh <kyle@ydangleapps.com>
2018-10-15 14:31:48 +0200

    changed import to require

--- src/internal/net/rest/api/UserApi.js
+++ src/internal/net/rest/api/UserApi.js
@@ -8,8 +8,8 @@
 //  ANY KIND, either express or implied. See the License for the specific language
 //  governing permissions and limitations under the License.
 //
-import urlParse from 'url-parse';
-import User from '../../../../model/User';
+const urlParse = require('url-parse');
+const User = require('../../../../model/User');
 
 
 export default class UserApi {
