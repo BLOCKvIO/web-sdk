a326fffc0d84c0002b1c2d3da540b999c3aaf821
Jack McIntosh <kyle@ydangleapps.com>
2019-03-27 17:38:17 +0200

    changed to es6 export

--- src/index.js
+++ src/index.js
@@ -8,8 +8,5 @@
 //  ANY KIND, either express or implied. See the License for the specific language
 //  governing permissions and limitations under the License.
 //
-const Blockv = require('./client/Blockv')
-const Discover = require('./client/Discover')
-
-module.exports = Blockv
-module.exports.Discover = Discover
+export { default } from './client/Blockv'
+export { default as Discover } from './client/Discover'
